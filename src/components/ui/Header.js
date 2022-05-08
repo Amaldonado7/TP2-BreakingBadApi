ab634b004dd34d3ec15d377958a482faaa47aa04
Ariadna <ariadnanahir.maldonado@gmail.com>
2022-05-08 16:45:59 -0300

    adding header, search and loading functionality connected to the API

---
+++ src/components/ui/Header.js
@@ -0,0 +1,11 @@
+import logo from '../../img/logo.png';
+
+const Header = () => {
+  return (
+    <header className='center'>
+      <img src={logo} alt='Imagen del logo de Braking Bad' />
+    </header>
+  )
+}
+
+export default Header;
